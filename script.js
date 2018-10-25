88971459d942edec1630fba6a92dc8c02f480992
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-10-25 16:58:55 +0200

    menu works with transition

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const mainNavToggle = document.querySelector('.main-nav__toggle');
 const navigation = document.querySelector('.navigation');
 
+navigationHeight = navigation.clientHeight;
+navigation.classList.add('main-nav__navigation--collapsed')
+
 mainNavToggle.addEventListener(
     'click',
     (event) =>{
         event.preventDefault();
         if(navigation.classList.contains('main-nav__navigation--collapsed')){
             navigation.classList.remove('main-nav__navigation--collapsed');
+            navigation.style.maxHeight = navigationHeight + "px";
         }else{
             navigation.classList.add('main-nav__navigation--collapsed');
         }
