f1731ee19a24a4292f4973d9c9609238ba10827c
MarcinGal <marcin921125@gmail.com>
2018-10-28 15:34:15 +0100

    local storage added

---
+++ cookies.js
@@ -0,0 +1,13 @@
+if(localStorage.getItem('person', 'visited')){
+    document.getElementById('cookies').style.display = 'none';
+    document.getElementById('cookies__button').style.display = 'none';
+}
+
+function buttonClick(){
+    localStorage.setItem('person', 'visited');
+    document.getElementById('cookies').style.display = 'none';
+    document.getElementById('cookies__button').style.display = 'none';
+ }
+
+
+document.getElementById('cookies__button').addEventListener('click', buttonClick);
