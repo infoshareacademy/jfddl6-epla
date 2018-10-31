92f2bacea515e99fdc938025a90c8f98d6ec9b50
marcingal <marcin921125@gmail.com>
2018-10-31 16:14:02 +0100

    instruction to do added

---
+++ js/carousel.js
@@ -0,0 +1,15 @@
+
+const carousel = function (){
+    alert('Hi there');
+    const slider = document.querySelector('.section-features__header');
+}
+
+
+
+window.setInterval(carousel, 2000);
+
+//Opacity 0 -> 1;
+//Kilka zdjęć ma  byc nalożonych na siebie;
+//wraz z Intervalem wybrać wszystkie;
+//wszystkim prócz jednego nadawać opacity 0, a jednemu opacity 1;
+//nadać transition i śmiga
